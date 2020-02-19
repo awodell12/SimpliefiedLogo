77424f522f9aed20effc516db3d7e84124818cf8
Samuel Thompson <stt13@duke.edu>
2020-02-19 00:29:42 -0500

    added interfaces for ModelListener and Command, commented both

---
+++ src/slogo/Command.java
@@ -0,0 +1,18 @@
+package slogo;
+
+/**
+ * Carries out the functionality of individual instructions
+ * in the programming language used (SLogo in this case).
+ * Implemented/Extended by concrete commands like "Forward".
+ */
+public interface Command {
+
+  /**
+   * Carries out the command, changing the relevant data in the model according to the
+   * effects of the command. Currently, commands can effect the turtle, the paths list,
+   * the variables, and the user defined commands. The effects are documented and returned.
+   * @return The effects on the model of this individual command, bundled into a CommandResult
+   * instance.
+   */
+  public CommandResult execute();
+}
