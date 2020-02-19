7f393df04363f154ae592ff705fb0d1f9b32859e
Cary Shindell <css57@duke.edu>
2020-02-19 18:40:00 -0500

    added method signatures and instance variables in Visualizer, and started coding some methods in visualizer and commandbox

--- src/slogo/FrontEnd/History.java
+++ src/slogo/FrontEnd/History.java
@@ -1,18 +1,36 @@
 package slogo.FrontEnd;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.control.Button;
+import javafx.scene.shape.Rectangle;
+
 /**
  * This class is used to manage the display elements of the historu (i.e. commands that have been run)
  */
 public class History {
 
-  /**
-   * Removes all entries from the History and its display
-   */
+    public History(Group root, Rectangle historyShape, Rectangle clearButtonShape){
+
+      Button clearButton = new Button("Clear", clearButtonShape);
+      clearButton.setOnAction(new EventHandler<>() {
+        @Override
+        public void handle(ActionEvent event) {
+          clearHistory();
+        }
+      });
+      root.getChildren().add(clearButton);
+    }
+
+    /**
+     * Removes all entries from the History and its display
+     */
     void clearHistory(){};
 
-  /**
-   * Takes in the latest user entry and stores it into history so it can be displayed.
-   * @param userInput The String copy of what the user enters into the CommandBox
-   */
+    /**
+     * Takes in the latest user entry and stores it into history so it can be displayed.
+     * @param userInput The String copy of what the user enters into the CommandBox
+     */
     void addHistoryEntry(String userInput){};
 }
