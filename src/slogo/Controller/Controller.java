9eee26a0a899c0ac437dd6bc3cee49bd9c99f0ee
Austin Odell <awo6@duke.edu>
2020-02-23 14:34:42 -0500

    Created a controller that has main to launch the application

---
+++ src/slogo/Controller/Controller.java
@@ -0,0 +1,21 @@
+package slogo.Controller;
+
+import static javafx.application.Application.launch;
+
+import java.io.IOException;
+import javafx.application.Application;
+import slogo.CommandResult;
+import slogo.FrontEnd.Visualizer;
+
+public class Controller implements ModelListener {
+  private static Visualizer myVisualizer;
+  @Override
+  public void handleModelUpdate(CommandResult update) {
+
+  }
+
+  public static void main (String[] args) {
+    Application.launch(Visualizer.class, args);
+  }
+
+}
