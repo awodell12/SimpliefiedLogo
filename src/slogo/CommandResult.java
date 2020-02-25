5a3ebac3efd3aa33a9d4207e1f83f86f60a72c98
James Rumsey <jpr31@duke.edu>
2020-02-24 20:07:37 -0500

    Constructor for command result modified

--- src/slogo/CommandResult.java
+++ src/slogo/CommandResult.java
@@ -1,14 +1,46 @@
 package slogo;
 
+import java.util.Map.Entry;
+
+
 public class CommandResult {
   private double returnVal;
   private int myTokensParsed;
   private String myErrorMessage;
 
-  public CommandResult(double retVal, int tokensParsed) {
+  private double myRotation;
+  private Entry<Double, Double> myPosition;
+  private String myVariableName;
+  private double myVariableValue;
+  private String myUDCName;
+  private String myUDCText;
+  private boolean myScreenClear;
+  private boolean myPenUp;
+  private boolean myTurtleVisible;
+  private boolean myTurtleReset;
+  //TODO: how to pass in path(s)?
+
+  public CommandResult(double retVal, int tokensParsed, double heading, Entry<Double, Double> pos,
+                       String variableName, double variableVal, String udcName, String udcText,
+                       boolean clearScreen, boolean penUp, boolean turtleVisible, boolean turtleReset) {
     returnVal = retVal;
     myTokensParsed = tokensParsed;
     myErrorMessage = "";
+
+    myRotation = heading;
+    myPosition = pos;
+    myVariableName = variableName;
+    myVariableValue = variableVal;
+    myUDCName = udcName;
+    myUDCText = udcText;
+    myScreenClear = clearScreen;
+    myPenUp = penUp;
+    myTurtleVisible = turtleVisible;
+    myTurtleReset = turtleReset;
+  }
+
+  public void setRotation(){
+
   }
 
   public void setErrorMessage(String msg) {
