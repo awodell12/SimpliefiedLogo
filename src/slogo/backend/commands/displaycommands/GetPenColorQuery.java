ea0e8cf37f5fac9c438964eb3b8752522a745b25
James Rumsey <jpr31@duke.edu>
2020-03-04 16:22:40 -0500

    Shape and pencolor queries implemented

---
+++ src/slogo/backend/commands/displaycommands/GetPenColorQuery.java
@@ -0,0 +1,37 @@
+package slogo.backend.commands.displaycommands;
+
+
+import java.util.List;
+import slogo.backend.Command;
+import slogo.backend.BackEndInternal;
+import slogo.CommandResult;
+import slogo.backend.CommandResultBuilder;
+
+public class GetPenColorQuery implements Command {
+
+    private static final int NUM_ARGS = 0;
+    private static final int NUM_VARS = 0;
+
+    @Override
+    public int getNumArgs() {
+        return NUM_ARGS;
+    }
+
+    @Override
+    public int getNumVars() {
+        return NUM_VARS;
+    }
+
+    @Override
+    public List<CommandResult> execute(List<Double> arguments,  List<String> vars, String[] tokens, BackEndInternal backEnd) {
+        CommandResultBuilder builder = backEnd.startCommandResult(backEnd.getTurtles().get(0).getHeading(), backEnd.getTurtles().get(0).getPosition());
+        builder.retVal(backEnd.getPathColor());
+        return List.of(builder.buildCommandResult());
+    }
+
+    @Override
+    public List<String> findVars(String[] tokenList) {
+        return null;
+    }
+
+}
