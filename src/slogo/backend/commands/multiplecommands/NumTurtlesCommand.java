196ab4d6e6b2bbeaf9ea329707806bb7df161ccd
James Rumsey <jpr31@duke.edu>
2020-03-05 16:27:09 -0500

    Small error with construtor in NumTurtles

--- src/slogo/backend/commands/multiplecommands/NumTurtlesCommand.java
+++ src/slogo/backend/commands/multiplecommands/NumTurtlesCommand.java
@@ -3,6 +3,7 @@ package slogo.backend.commands.multiplecommands;
 import slogo.CommandResult;
 import slogo.backend.BackEndInternal;
 import slogo.backend.Command;
+import slogo.backend.Interpreter;
 import slogo.backend.ParseException;
 
 import java.util.List;
@@ -20,7 +21,7 @@ public class NumTurtlesCommand implements Command {
 
     @Override
     public List<CommandResult> execute(List<Double> arguments, List<String> vars, String[] tokens,
-                                       BackEndInternal backEnd) throws ParseException {
+                                       BackEndInternal backEnd, Interpreter interpreter) throws ParseException {
         return List.of(backEnd.makeCommandResult(backEnd.getTurtles().size(),0));
     }
 
