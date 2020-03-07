d078c8905c6abfbce5e4704be8e3027e6f2411a3
James Rumsey <jpr31@duke.edu>
2020-03-06 23:19:22 -0500

    fixed findVars in make

--- src/slogo/backend/commands/controlandvariables/MakeCommand.java
+++ src/slogo/backend/commands/controlandvariables/MakeCommand.java
@@ -18,11 +18,12 @@ public class MakeCommand extends Command {
   @Override
   public List<CommandResult> execute(List<Double> arguments, List<String> vars, String[] tokens,
       BackEndInternal backEnd, Interpreter interpreter) throws ParseException {
+    System.out.println(vars.get(0));
     backEnd.setVariable(vars.get(0),arguments.get(0));
     CommandResultBuilder builder = backEnd.startCommandResult(arguments.get(0));
     return List.of(builder.buildCommandResult());
   }
 
   @Override
-  public List<String> findVars(String[] tokenList) {return List.of(tokenList[0].substring(1));}
+  public List<String> findVars(String[] tokenList) {return List.of(tokenList[0]);}
 }
